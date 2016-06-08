658a55783db7225f4880e3274986bf1b3a51be3b
rangertaha <tahasaidi@gmail.com>
2016-06-08 17:32:25 -0400

    initial

--- urllockdown/admin.py
+++ urllockdown/admin.py
@@ -5,5 +5,32 @@ import logging
 
 from django.contrib import admin
 
+from .models import Access, URL, LockPage
+
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
+
+
+@admin.register(LockPage)
+class LockPageAdmin(admin.ModelAdmin):
+    list_display = ('title', )
+    search_fields = ('id', 'title', 'text', 'error')
+    ordering = ['id', 'title', 'text', 'error']
+    list_display_links = ('title',)
+    list_editable = ('title', 'text', 'error')
+
+
+@admin.register(URL)
+class URLAdmin(admin.ModelAdmin):
+    list_display = ('pattern', )
+    search_fields = ('id', 'pattern')
+    ordering = ['id', 'pattern']
+    list_display_links = ('pattern',)
+
+
+@admin.register(Access)
+class AccessAdmin(admin.ModelAdmin):
+    list_display = ('password', )
+    search_fields = ('id', 'password')
+    list_display_links = ('password',)
+
--- urllockdown/models.py
+++ urllockdown/models.py
@@ -6,3 +6,21 @@ from django.utils.translation import ugettext_lazy as _
 from django.template.defaultfilters import slugify
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
+
+
+class LockPage(models.Model):
+    title = models.CharField(_('Title'), max_length=512, blank=True, null=True)
+    text = models.TextField(_('Text'), blank=True, null=True)
+    error = models.TextField(_('Error Message'), blank=True, null=True)
+
+    def __unicode__(self):
+        return self.title
+
+
+class URL(models.Model):
+    pattern = models.CharField(_('URL Pattern'), max_length=512)
+    password = models.CharField(_('Password'), max_length=512)
+    lockpage = models.ForeignKey(LockPage)
+
+    def __unicode__(self):
+        return self.pattern
